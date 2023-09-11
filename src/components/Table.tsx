fed5bdb9293b7ce4d5ea685fc97053e8cbb6493d
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-09-11 07:26:53 -0300

    requisito 5 completo

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,47 @@
-function Table() {
-  return <div>Table</div>;
-}
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../types';
 
-export default Table;
+export default function Table() {
+  const expenses = useSelector((state: RootState) => state.wallet.expenses);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ parseFloat(expense.value).toFixed(2) }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                { Number(expense.value * expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2) }
+              </td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 function Wallet() {
   return (
     <div>
       <Header />
       <WalletForm />
+      <Table />
     </div>
   );
 }
